09197bdd212506b04e2fe31475ac0c41c3bd9221
Zel0DK <Rasmus.Christensen23@gmail.com>
2024-11-17 15:35:22 +0100

    Added maps and pictures

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,7 +3,7 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search: React.FC = () => {
-  return <DocSearch appId="W6V9JVS5SU" apiKey="3eb4544013afecb38b276148364bf788" indexName="stausi" />;
+  return <DocSearch appId="" apiKey="" indexName="" />;
 };
 
 export default Search;
